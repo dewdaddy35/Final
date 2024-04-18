2f5f837312e4bf8bcbdc816c2681555304937fc0
Sergio Inzunza <inxunxa@gmail.com>
2024-04-17 18:26:22 -0700

    Add UserRegister

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomUserSerializer
+from rest_framework import viewsets
+from .models import CustomUser
+from rest_framework.decorators import action, api_view
 
-# Create your views here.
+class UserAPIView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User successfully registered."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def user_register(request):
+    data = request.data
+
+    email = data.get("email")   
+    serializer = CustomUserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
+
