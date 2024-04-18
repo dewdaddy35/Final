fa9f8180d8734e85482fa2a377439f442c508cf8
Sergio Inzunza <inxunxa@gmail.com>
2024-04-17 18:27:56 -0700

    Fix

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -6,15 +6,6 @@ from rest_framework import viewsets
 from .models import CustomUser
 from rest_framework.decorators import action, api_view
 
-class UserAPIView(APIView):
-    def post(self, request):
-        serializer = CustomUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User successfully registered."}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 @api_view(["POST"])
 def user_register(request):
     data = request.data
